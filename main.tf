d8132db8767b7624534936c82be0a270d2324cba
umihico <umihico@users.noreply.github.com>
2022-03-13 10:30:49 +0900

    style: refactor

--- main.tf
+++ main.tf
@@ -1,3 +1,5 @@
+data "aws_caller_identity" "self" {}
+
 resource "aws_kms_key" "shared" {
   description              = "Many projects depend on this key. Please do not delete this."
   key_usage                = "ENCRYPT_DECRYPT"
@@ -5,6 +7,21 @@ resource "aws_kms_key" "shared" {
   enable_key_rotation      = false # rotation costs money
   is_enabled               = true
   multi_region             = false
+  policy = jsonencode({
+    "Id" : "key-default-1",
+    "Statement" : [
+      {
+        "Action" : "kms:*",
+        "Effect" : "Allow",
+        "Principal" : {
+          "AWS" : "arn:aws:iam::${data.aws_caller_identity.self.account_id}:root"
+        },
+        "Resource" : "*",
+        "Sid" : "Enable IAM User Permissions"
+      }
+    ],
+    "Version" : "2012-10-17"
+  })
 }
 
 resource "aws_kms_alias" "shared" {
